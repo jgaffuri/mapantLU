d1d496a45f772ae7b01f91dd20252c078ec90fc4
jgaffuri <jgaf@proton.me>
2023-04-14 10:12:14 +0200

    a

--- code/java/src/main/java/org/luxoc/mapantlux/A1Download.java
+++ code/java/src/main/java/org/luxoc/mapantlux/A1Download.java
@@ -34,8 +34,8 @@ public class A1Download {
 		ArrayList<Feature> ziptiles = GeoData.getFeatures("/home/juju/Bureau/workspace/mapantLU/code/qgis/lidar_urls.gpkg");
 		LOGGER.info(ziptiles.size());
 
-		int xMin = 61400;
-		int xMax = 64400;//;51500
+		int xMin = 64000;
+		int xMax = 66000;//;51500
 		int yMin = 0;
 		int yMax = 100000;//; 95500
 
