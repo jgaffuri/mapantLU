80b528fd4131f76f2c5015861820ab227aee8124
jgaffuri <jgaf@proton.me>
2023-05-03 18:40:08 +0200

    a

--- code/java/src/main/java/org/luxoc/mapantlux/AZProcess.java
+++ code/java/src/main/java/org/luxoc/mapantlux/AZProcess.java
@@ -6,7 +6,8 @@ package org.luxoc.mapantlux;
 
 public class AZProcess {
 
-	public static void main() throws Throwable {
+	
+	public static void main(String[] args) throws Throwable {
 		A2ToRGB.main(null);
 		A3Merge.main(null);
 	}
